54596a95c9f7e46fa30a443525da0c5bc05a9853
Ghazal Minaei <ghminaei1037@gmail.com>
2021-04-23 22:46:09 +0430

    Chore: delete unwanted api

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -1,7 +1,7 @@
 import { HelloService } from './hello.service';
 import { Controller, Post, Body, Get, Header, Query } from '@nestjs/common';
 import { PersonDto } from './dto/person.dto';
-import { ApiResponse, ApiBearerAuh, ApiQuery } from '@nestjs/swagger'
+import { ApiResponse, ApiBearerAuth, ApiQuery } from '@nestjs/swagger'
 
 @Controller('hello')
 export class HelloController {
